f4b3d9692bbf91c3c41fb5b39daefbb9f4f3d59b
Usman Zahid <usman@usmanhostyeet.localdomain>
2018-10-30 13:13:48 -0700

    make setup, env setup

---
+++ src/h1bCounting.java
@@ -0,0 +1,15 @@
+//Usman Zahid
+//2019 Insight Data Engineer Coding Challenge
+
+import java.util.*;
+
+
+
+public class h1bCounting{
+
+	public static void main(String[] args){
+
+		System.out.println("this");
+	}
+}
+
